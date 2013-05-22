cfb1659eaa774379f07246a59dcf81d45b28893b
rrebucas <renante_rebucas@yahoo.com>
2013-05-22 13:32:26 +0800

    loading page.......

--- files/ps_xls_import.php
+++ files/ps_xls_import.php
@@ -1,6 +1,8 @@
 <?php
 
-include 'ps_connect_db.php';
+require_once 'ps_connect_db.php';
+
+
 
 ?>
 
@@ -17,36 +19,99 @@ include 'ps_connect_db.php';
 <link href="ps_includes/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
 
 <!--ps js-->
- <script src="ps_theme/js/jquery.js"></script>
+ <script src="ps_theme/js/jquery-1.9.1.min.js"></script>
+
  <script src="ps_includes/bootstrap/js/bootstrap.min.js"></script>
  <script type="text/javascript" src="ps_theme/js/modernizr.custom.58301.js"></script>
 
+ <script>
+
+$(document).ready(function(){
+
+    $(window).load(function() {
+
+    	$("#loading").fadeOut('fast', function () {
+    $("#body-wrapper").fadeIn(3000);
+  });
+    	
+
+    	 })
+
+
+});
+</script>
+
 <style type="text/css">
-body {
+#body-wrapper {
 	font-family:verdana,arial,sans-serif;
 	font-size:10pt;
 	margin:30px;
 	background-color:#fff;
 	
 	}
+#loading {
+
+position: fixed;
+width: 100%;
+height: 100%;
+padding-top: 15%;
+background: #fff;
+z-index: 100;
+overflow: hidden;
+text-align: center;
+}
 </style>
 </head>
 <body>
+
+<div id="loading"><img src="ps_theme/images/ajax-loader.gif" /></div>
+<div id="body-wrapper">
+
 <h2>Import</h2>
+
+<?php   
+
+if (!isset($_FILES['file_import'])) {
+
+?>
 <p> <span class="label label-info">Howdy!</span> Upload your <strong>Biometric Excel</strong> file and we’ll import the <abbr title="Employees Name">employees name</abbr>, <abbr title="Employees ID">employees id</abbr>, <abbr title="Employees Date and Time">employees date-time</abbr>  and <abbr title="Employees Status">employees status</abbr>  into this site.</p>
 <p>Choose a <strong>Microsoft Excel</strong>  ( <code>.xlsx</code> or <code>.xls</code> )file to upload, then click import.</p>
-	<form enctype="multipart/form-data" id="import-upload-form" method="post" class="wp-upload-form" action="admin.php?import=wordpress&amp;step=1&amp;_wpnonce=af6a9f48c9">
+
+
+	<form enctype="multipart/form-data" id="import-upload-form" method="post" class="wp-upload-form" action="#">
 		<p>
 		<label for="upload">Choose a file from your computer:</label> (Maximum size: 2 MB)
-		<input type="file" id="upload" name="import" size="25">
-		<input type="hidden" name="action" value="save">
-		<input type="hidden" name="max_file_size" value="15728640">
+		<input type="file" id="upload" name="file_import" size="25">
+		
 		</p>
 		<p class="submit">
 			<input type="submit" name="submit" id="submit" class="btn btn-small btn-primary" value="Import File">
 		</p>
 	</form>
+<?php
+}
 
+else {
+
+	$error = array();
+	$allowed_ext = array('jpg','jpeg','png','gif');
+
+	$file_name = $_FILES['file_import']['name'];
+	$file_ext= explode('.', $file_name);
+	$file_size = $_FILES['file_import']['size'];
+	$file_tmp = $_FILES['file_import']['tmp_name'];
+
+
+	
+
+
+
+}
+
+
+
+
+?>
 
 
 <!--page generate-->
@@ -58,5 +123,11 @@ $time_gen = number_format(microtime(true) - $start_time, 9);
 This page was generated in <span class="atv"><?php echo"$time_gen"; ?></span> seconds.
 </pre>
 
+
+</div>
 </body>
 </html>
+
+<?php
+mysqli_close($con);
+?>
