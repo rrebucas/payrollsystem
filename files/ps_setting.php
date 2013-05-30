b7eb4979aa0e9348d9d6cc9b6e43ef488b8d7e64
rudampjr <rudampjr@yahoo.com>
2013-05-30 09:35:45 +0800

    update rudy: new-user, login page, settings

---
+++ files/ps_setting.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<link href="ps_includes/bootstrap/css/bootstrap.css" rel="stylesheet" media="screen"/>
+<link rel="stylesheet" href="ps_includes/bootstrap/css/bootstrap-responsive.css"></link>
+<script type="text/javascript" src="ps_includes/bootstrap/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+<div id="settings" class="container-fluid">
+	<h1>Settings</h1>
+	<div id="user_settings" class="container-fluid">
+		<form>
+			<a href="ps_new_user.php" class="btn btn-primary btn-large">
+				Add New User
+			</a>
+		</form>
+
+
+
+     </div>
+
+
+<div class="clear"></div>
+<!--Page Generated-->
+<?php $start_time = microtime(true);
+$time_gen = number_format(microtime(true) - $start_time, 9);
+?>
+
+<pre class="prettyprint" id="page=generated" style="width: 90%; clear: both; margin: 0 auto; margin-bottom: 20px;">
+ This page was generated in <span class="atv"><?php echo"$time_gen"; ?>
+</span> seconds.
+</pre>
+
+
+</body>
+
+</html>
